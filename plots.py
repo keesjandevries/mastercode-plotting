28040ebd09b1105e399f7debb4bcc1ad9c481116
Sam Rogerson <samr@cern.ch>
2012-01-17 02:46:08 +0100

    updated to match namespace to new histograms file

--- plots.py
+++ plots.py
@@ -2,14 +2,15 @@
 
 import plot_list as pl
 import plot_options as po
+import histograms as hfuncs
 
 def main( argv=None ) :
     fileout = "sample_output.pdf"
     
     d, tree_props = pl.get_file_dict()
-    hd = pl.get_hist_dict( d )
+    hd = hfuncs.get_hist_dict( d )
 
-    hists = pl.get_filled_hists( hd, tree_props )
+    hists = hfuncs.get_filled_hists( hd, tree_props )
     po.print_to_single_file( hists, fileout )
 
 if __name__ == "__main__":
