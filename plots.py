3e7c150bc0e451c4d8ead960ac27db92f4dd9b52
Sam Rogerson <samr@cern.ch>
2012-01-29 18:01:50 +0100

    changed default behavior to write the histograms out ot a root file instead of drawing

--- plots.py
+++ plots.py
@@ -1,17 +1,17 @@
 #! /usr/bin/env python
 
 import plot_list as pl
-import plot_options as po
 import histograms as hfuncs
 
 def main( argv=None ) :
-    fileout = "sample_output.pdf"
+    fileout = "sample_output.root"
     
     d, tree_props = pl.get_file_dict()
     hd = hfuncs.get_hist_dict( d )
 
     hists = hfuncs.get_filled_hists( hd, tree_props )
-    po.print_to_single_file( hists, fileout )
+
+    hfuncs.save_hlist_to_root_file( hists, fileout)
 
 if __name__ == "__main__":
     main()
