7354bf09c2900ab9bc1f3142c9f0c9c927ef5e7b
Sam Rogerson <samr@cern.ch>
2012-01-31 15:03:43 +0100

    various extra options tested

--- plotSpace.py
+++ plotSpace.py
@@ -17,15 +17,26 @@ hists = [
             f.get("data_histograms/iHist_1_2_dchi"),
         ]
 
-fig = plt.figure(figsize=[8,5])
+fig = plt.figure(figsize=[18,5])
 
 mins = [ 20.0, 0.0, 0.0 ]
 maxs = [ 30.0, .20, 25. ]
 
+title = [ r"$\chi^{2}$", r"$P(\chi^{2},N_{DOF})$",r"$\Delta\chi^{2}$", ] 
+levels = [ [ 22.23, 25.99 ], [ 0.05, 0.10 ], [ 2.23, 5.99 ], ]
+colors = [ [ 'r', 'b' ],     [ 'r', 'b' ],   [ 'r', 'b' ], ]
+xaxis_label= r"$m_{0} [GeV/c^{2}]$"
+yaxis_label= r"$m_{1/2} [GeV/c^{2}]$"
+
+ax_list = []
 for i in range(3) :
-    fig.add_subplot(1, 3, i+1 )
+    ax_list.append( fig.add_subplot(1, 3, i+1 ) )
+    ax_list[-1].set_xlabel( xaxis_label )
+    ax_list[-1].set_ylabel( yaxis_label )
+    #plt.axis([0,1000,0,1000])
+    hists[i].contour( levels=levels[i], colors = colors[i], extent = (0,1500,0,1500), origin = None )
     hists[i].colz()
     plt.clim(mins[i],maxs[i])
 
-plt.show()
-#plt.savefig("test.pdf")
+#plt.show()
+plt.savefig("test.png")
