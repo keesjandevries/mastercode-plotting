414e974d1143923b8704601a855b9520efd2aba4
Sam Rogerson <samr@cern.ch>
2012-04-18 10:56:05 +0200

    only do 1d

--- plots.py
+++ plots.py
@@ -4,10 +4,10 @@ from modules import spaces as s
 from modules import lh1d as lh
 
 def main() :
-    spaces = hl.getSpaceDict()
+#    spaces = hl.getSpaceDict()
     histos = hl.get1DDict()
-    s.makeSingleSpacePlot(spaces)
-    s.makeGridPlots(spaces)
+#    s.makeSingleSpacePlot(spaces)
+#    s.makeGridPlots(spaces)
     lh.makeSingle1DPlot(histos)
 
 if __name__ == "__main__":
