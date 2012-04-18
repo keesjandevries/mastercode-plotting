684b6151aafa34c8f96501d41abd9797fb95e943
Sam Rogerson <samr@cern.ch>
2012-04-18 17:57:07 +0200

    rename import

--- config/histo_list.py
+++ config/histo_list.py
@@ -1,4 +1,4 @@
-from defaults.default_plots import getDefaults as pds
+from defaults.default_plots import getDefaults as dp
 
 def getSpaceDict() :
     return spaces
@@ -8,18 +8,18 @@ def get1DDict() :
 
 
 hists = {
-            "data_histograms/iHist_119_dchi" : pds( "dchi", "neu1" ),
-            "data_histograms/iHist_28_dchi" : pds( "dchi", "mh" )
+            "data_histograms/iHist_119_dchi" : dp( "dchi", "neu1" ),
+            "data_histograms/iHist_28_dchi" : dp( "dchi", "mh" )
         }
 
 spaces = { 
-            "data_histograms/iHist_1_2_chi2" : pds("chi2" , "m0",  "m12" ),
-            "data_histograms/iHist_1_2_pval" : pds("pval" , "m0",  "m12" ),
-            "data_histograms/iHist_1_2_dchi" : pds("dchi" , "m0",  "m12" ),
-            "data_histograms/iHist_1_4_chi2" : pds("chi2" , "m0",  "tanb"),
-            "data_histograms/iHist_1_4_pval" : pds("pval" , "m0",  "tanb"),
-            "data_histograms/iHist_1_4_dchi" : pds("dchi" , "m0",  "tanb"),
-            "data_histograms/iHist_4_2_chi2" : pds("chi2" , "tanb","m12" ),
-            "data_histograms/iHist_4_2_pval" : pds("pval" , "tanb","m12" ),
-            "data_histograms/iHist_4_2_dchi" : pds("dchi" , "tanb","m12" ),
+            "data_histograms/iHist_1_2_chi2" : dp("chi2" , "m0",  "m12" ),
+            "data_histograms/iHist_1_2_pval" : dp("pval" , "m0",  "m12" ),
+            "data_histograms/iHist_1_2_dchi" : dp("dchi" , "m0",  "m12" ),
+            "data_histograms/iHist_1_4_chi2" : dp("chi2" , "m0",  "tanb"),
+            "data_histograms/iHist_1_4_pval" : dp("pval" , "m0",  "tanb"),
+            "data_histograms/iHist_1_4_dchi" : dp("dchi" , "m0",  "tanb"),
+            "data_histograms/iHist_4_2_chi2" : dp("chi2" , "tanb","m12" ),
+            "data_histograms/iHist_4_2_pval" : dp("pval" , "tanb","m12" ),
+            "data_histograms/iHist_4_2_dchi" : dp("dchi" , "tanb","m12" ),
          }
