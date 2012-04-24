316a76b7704b98ab0bd7297727f97076261d8045
Sam Rogerson <samr@cern.ch>
2012-04-24 14:21:37 +0200

    rename mneu1

--- config/histo_list.py
+++ config/histo_list.py
@@ -8,8 +8,8 @@ def get1DDict() :
 
 
 hists = {
-            "data_histograms/iHist_119_dchi" : pd( "dchi", "neu1" ),
-            "data_histograms/iHist_28_dchi" : pd( "dchi", "mh" )
+            "data_histograms/iHist_mneu1_dchi" : pd( "dchi", "mneu1" ),
+            "data_histograms/iHist_mh_dchi"    : pd( "dchi", "mh" )
         }
 
 spaces = { 
