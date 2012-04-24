3f06f3dd10d84a1237f3704daaf10ea045fd35fc
Sam Rogerson <samr@cern.ch>
2012-04-24 11:04:29 +0200

    remove containers: direct option values

--- config/default_plots.py
+++ config/default_plots.py
@@ -4,12 +4,6 @@ def getDefaults( mode, *axes ) :
     return eval( format_string % tuple( list(axes) + [mode] ) )
 
 # per value
-m0m12Base   = { "xticks" : ticks["m0"],   "yticks" : ticks["m12"],  }
-m0tanbBase  = { "xticks" : ticks["m0"],   "yticks" : ticks["tanb"], }
-tanbm12Base = { "xticks" : ticks["tanb"], "yticks" : ticks["m12"],  }
-neu1Base    = { "xticks" : ticks["neu1"], } 
-mhBase = { "xticks" : ticks["mh"], } 
-
 ticks = { 
           "m0"   : 500,
           "m12"  : 500,
@@ -18,36 +12,33 @@ ticks = {
           "mh"   : 5,
         }
 
+m0m12Base   = { "xticks" : ticks["m0"],   "yticks" : ticks["m12"],  }
+m0tanbBase  = { "xticks" : ticks["m0"],   "yticks" : ticks["tanb"], }
+tanbm12Base = { "xticks" : ticks["tanb"], "yticks" : ticks["m12"],  }
+neu1Base    = { "xticks" : ticks["neu1"], } 
+mhBase = { "xticks" : ticks["mh"], } 
 
 # per type
-
-colors = [ 'r', 'b' ]
-clevels = { 
-              "dchi" : [2.23,5.99],
-              "pval" : [0.05,0.10],
-          }
+colors  = [ 'r', 'b' ]
 
 chi2Base = { 
                "title"    : r"$\chi^{2}$",
-               "contours" : cLevelDelta(20.), 
+               "contours" : [22.23, 25.99], # need more sensible values
                "colors"   : colors, 
                "zrange"   : [20., 45.]
            }
 
 pvalBase = {
                 "title"    : r"$P(\chi^{2},N_{DOF})$",
-                "contours" : clevels["pval"],
+                "contours" : [0.05, 0.1],
                 "colors"   : colors,
                 "zrange"   : [0., 0.2],
            }
 
 dchiBase = {
                 "title"    : r"$\Delta\chi^{2}$",
-                "contours" : clevels["dchi"],
+                "contours" : [2.23, 5.99],
                 "colors"   : colors,
                 "zrange"   : [0., 25.],
                 "zrange1d" : [0., 9.],
            }
-
-def cLevelDelta( delta = 0.0 ) :
-    return [ x+delta for x in clevels["dchi"] ] 
