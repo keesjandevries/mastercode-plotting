1dacd0b1113d7548d03ad85a1966180714ea4ad3
Sam Rogerson <samr@cern.ch>
2012-05-16 18:11:34 +0200

    name update, and use proper dicts not eval

--- config/plot_defaults.py
+++ config/plot_defaults.py
@@ -1,7 +1,7 @@
 def getDefaults( mode, *axes ) :
-    axes_format = "%s"*len(axes)
-    format_string = "dict(" + axes_format + "Base.items() +%sBase.items())"
-    d = eval( format_string % tuple( list(axes) + [mode] ) )
+    d = {}
+    d.update(spaceBases[ "".join(axes) ])
+    d.update( typeBases[mode] )
     d["short_names"] = list(axes)
     d["mode"] = mode
     return d
@@ -11,37 +11,41 @@ ticks = {
           "m0"    : 500,
           "m12"   : 500,
           "tanb"  : 10,
-          "mneu1" : 100,
-          "mh"    : 5,
+          "neu1"  : 100,
+          "m_h^0" : 5,
         }
 
-m0m12Base   = { "xticks" : ticks["m0"],   "yticks" : ticks["m12"],  }
-m0tanbBase  = { "xticks" : ticks["m0"],   "yticks" : ticks["tanb"], }
-tanbm12Base = { "xticks" : ticks["tanb"], "yticks" : ticks["m12"],  }
-mneu1Base   = { "xticks" : ticks["mneu1"], }
-mhBase      = { "xticks" : ticks["mh"], }
+spaceBases = {
+    "m0m12"   : { "xticks" : ticks["m0"],   "yticks" : ticks["m12"],  },
+    "m0tanb"  : { "xticks" : ticks["m0"],   "yticks" : ticks["tanb"], },
+    "tanbm12" : { "xticks" : ticks["tanb"], "yticks" : ticks["m12"],  },
+    "neu1"    : { "xticks" : ticks["neu1"], },
+    "m_h^0"   : { "xticks" : ticks["m_h^0"], },
+}
 
 # per type
 colors  = [ 'r', 'b' ]
 
-chi2Base = { 
-               "title"    : r"$\chi^{2}$",
-               "contours" : [22.23, 25.99], # need more sensible values
-               "colors"   : colors, 
-               "zrange"   : [20., 45.]
-           }
+typeBases = {
+    "chi2" : { 
+        "title"    : r"$\chi^{2}$",
+        "contours" : [22.23, 25.99], # need more sensible values
+        "colors"   : colors, 
+        "zrange"   : [20., 45.]
+    },
 
-pvalBase = {
-                "title"    : r"$P(\chi^{2},N_{DOF})$",
-                "contours" : [0.05, 0.1],
-                "colors"   : colors,
-                "zrange"   : [0., 0.2],
-           }
+    "pval" : {
+        "title"    : r"$P(\chi^{2},N_{DOF})$",
+        "contours" : [0.05, 0.1],
+        "colors"   : colors,
+        "zrange"   : [0., 0.2],
+    },
 
-dchiBase = {
-                "title"    : r"$\Delta\chi^{2}$",
-                "contours" : [2.23, 5.99],
-                "colors"   : colors,
-                "zrange"   : [0., 25.],
-                "zrange1d" : [0., 9.],
-           }
+    "dchi" : {
+        "title"    : r"$\Delta\chi^{2}$",
+        "contours" : [2.23, 5.99],
+        "colors"   : colors,
+        "zrange"   : [0., 25.],
+        "zrange1d" : [0., 9.],
+    },
+}
