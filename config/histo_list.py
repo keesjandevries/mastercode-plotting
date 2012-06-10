3d3ac1e5cb8277e71a50c04cd7975407b168d447
Keesjan de Vries <keesjan777@gmail.com>
2012-06-10 11:55:47 +0100

    plots that I use now

--- config/histo_list.py
+++ config/histo_list.py
@@ -8,18 +8,35 @@ def get1DDict() :
 
 
 hists = {
-            "data_histograms/iHist_mneu1_dchi" : pd( "dchi", "mneu1" ),
-            "data_histograms/iHist_mh_dchi"    : pd( "dchi", "mh" )
+#            "data_histograms/iHist_neu1_dchi"      : pd( "dchi", "mneu1" ),
+#            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi", "mh" ),
+#            "data_histograms/iHist_stau_1_dchi"     : pd("dchi","mstau1"),
+#            "data_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
+#            "data_histograms/iHist_squark_r_dchi"  : pd("dchi", "msqr" ),
+#            "data_histograms/iHist_Bsmumu_dchi"     : pd("dchi", "bsmm" ),
         }
 
+#spaces = { 
+#            "data_histograms/iHist_m0_m12_chi2"   : pd("chi2", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_m12_pval"   : pd("pval", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_m12_dchi"   : pd("dchi", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_tanb_chi2"  : pd("chi2", "m0",  "tanb"),
+#            "data_histograms/iHist_m0_tanb_pval"  : pd("pval", "m0",  "tanb"),
+#            "data_histograms/iHist_m0_tanb_dchi"  : pd("dchi", "m0",  "tanb"),
+#            "data_histograms/iHist_tanb_m12_chi2" : pd("chi2", "tanb","m12" ),
+#            "data_histograms/iHist_tanb_m12_pval" : pd("pval", "tanb","m12" ),
+#            "data_histograms/iHist_tanb_m12_dchi" : pd("dchi", "tanb","m12" ),
+#         }
 spaces = { 
-            "data_histograms/iHist_m0_m12_chi2"   : pd("chi2", "m0",  "m12" ),
-            "data_histograms/iHist_m0_m12_pval"   : pd("pval", "m0",  "m12" ),
-            "data_histograms/iHist_m0_m12_dchi"   : pd("dchi", "m0",  "m12" ),
-            "data_histograms/iHist_m0_tanb_chi2"  : pd("chi2", "m0",  "tanb"),
-            "data_histograms/iHist_m0_tanb_pval"  : pd("pval", "m0",  "tanb"),
-            "data_histograms/iHist_m0_tanb_dchi"  : pd("dchi", "m0",  "tanb"),
-            "data_histograms/iHist_tanb_m12_chi2" : pd("chi2", "tanb","m12" ),
-            "data_histograms/iHist_tanb_m12_pval" : pd("pval", "tanb","m12" ),
-            "data_histograms/iHist_tanb_m12_dchi" : pd("dchi", "tanb","m12" ),
-         }
+#            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_m12_dX_Oh^2"     : pd("chi2", "m0",  "m12" ),
+            "data_histograms/iHist_m0_m12_pred_Bsmumu"     : pd("chi2", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "m12" ),
+#            "data_histograms/iHist_m0_tanb_chi2"    : pd("chi2", "m0",  "m12" ),
+#            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
+#            "data_histograms/iHist_tanb_m12_chi2"   : pd("chi2", "tanb","m12" ),
+#            "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
+#            "data_histograms/iHist_mA0_tanb_chi2"   : pd("chi2", "MA"  ,"tanb"),
+##            "data_histograms/iHist_mneu1_ssi_dchi": pd("dchi","mneu1","ssi" ),
+        }
