cc606f65bc69f45bc69380ecd4bb4a1af48e7842
Keesjan de Vries <keesjan777@gmail.com>
2012-06-17 16:03:48 +0100

    fix for make_colour_contour_overlay and make log axes possible

--- modules/spaces.py
+++ modules/spaces.py
@@ -44,9 +44,9 @@ def make_single_space_overlay( histos, filenames, ext="png" ) :
         for hist, lst in zip(hists, linestyles  ):
             hist.contour( levels = options["contours"], colors = options["colors"], linewidths = 3, linestyles=lst )
             x_i, y_i = find_minimum_2d(hist)
-            print x_i,y_i, hist.x[x_i] , hist.y[y_i], hist.xedges[x_i],hist.yedges[y_i]
+#            print x_i,y_i, hist.x[x_i] , hist.y[y_i], hist.xedges[x_i],hist.yedges[y_i]
 #            plt.plot([hist.xedges[x_i],hist.yedges[y_i]],'g',markersize=10 )
-            plt.plot([hist.x[x_i],hist.y[y_i]],'g' )
+            plt.scatter([hist.x[x_i],hist.y[y_i]],'g' )
 #            plt.plot([x_i,y_i],'g',markersize=100 )
 #            plt.plot([500,1000],'g',markersize=100 )
             # FIXME: HERE WE NEED THE GREEN STARS
@@ -54,7 +54,7 @@ def make_single_space_overlay( histos, filenames, ext="png" ) :
         pylab.yticks(pylab.arange( ymin, ymax*1.001, options["yticks"] ) )
         pylab.xticks(pylab.arange( xmin, xmax*1.001, options["xticks"] ) )
         axes.set_title( options["title"] )
-        plt.savefig( fig_name( options, filenames[0] ) + ".%s" % ext )
+        plt.savefig( fig_name( options, filenames[0] ) + "_overlay.%s" % ext )
 
 def makeSingleSpacePlot( histos, filename, ext="png" ) :
     f = r2m.RootFile(filename)
@@ -67,38 +67,44 @@ def makeSingleSpacePlot( histos, filename, ext="png" ) :
         axes = plt.axes()
         axes.set_xlabel( hist.xlabel )
         axes.set_ylabel( hist.ylabel )
+        if options.get("xlog",None) is not None:
+            axes.set_xscale('log')
+        if options.get("ylog",None) is not None:
+            axes.set_yscale('log')
         hist.contour( levels = options["contours"], colors = options["colors"], linewidths = 2 )
         hist.colz()
         plt.axis( [xmin, xmax, ymin, ymax] )
         plt.clim( *options["zrange"] )
-        pylab.yticks(pylab.arange( ymin, ymax+0.1, options["yticks"] ) )
-        pylab.xticks(pylab.arange( xmin, xmax+0.1, options["xticks"] ) )
+        # allow for not specifying x,ytick  
+        if options.get('xticks',None) is not None: 
+            pylab.xticks(pylab.arange( xmin, xmax*1.001, options["xticks"] ) )
+        if options.get('yticks',None) is not None: 
+            pylab.yticks(pylab.arange( ymin, ymax*1.001, options["yticks"] ) )
         axes.set_title( options["title"] )
         plt.savefig( fig_name( options, filename ) + ".%s" % ext )
 
 def make_colour_contour_overlay(colour,contour,filename, ext="png"):
     f = r2m.RootFile(filename)
-    for hname1, options1 in colour.iteritems() :
-        pass
-    for hname2, options2 in contour.iteritems() :
-        pass
-    hist1 = f.get(hname1)
-    hist2 = f.get(hname2)
-    fig = plt.figure( figsize=[8,6] )
-    xmin,xmax = hist1.xedges[0], hist1.xedges[-1]
-    ymin,ymax = hist1.yedges[0], hist1.yedges[-1]
-    plt.axis( [xmin, xmax, ymin, ymax] )
-    axes = plt.axes()
-    axes.set_xlabel( hist1.xlabel )
-    axes.set_ylabel( hist1.ylabel )
-    hist2.contour( levels = options2["contours"], colors = options2["colors"], linewidths = 2 )
-    hist1.colz()
-    plt.axis( [xmin, xmax, ymin, ymax] )
-    plt.clim( *options1["zrange"] )
-    pylab.yticks(pylab.arange( ymin, ymax+0.1, options1["yticks"] ) )
-    pylab.xticks(pylab.arange( xmin, xmax+0.1, options1["xticks"] ) )
-    axes.set_title( options1["title"] )
-    plt.savefig( fig_name( options1, filename ) + "_col_con.%s" % ext )
+    for col,cont in zip(colour,contour) :
+        hname1,  hname2=col[0],  cont[0]
+        options1, options2=col[1] , cont[1]
+        hist1 = f.get(hname1)
+        hist2 = f.get(hname2)
+        fig = plt.figure( figsize=[8,6] )
+        xmin,xmax = hist1.xedges[0], hist1.xedges[-1]
+        ymin,ymax = hist1.yedges[0], hist1.yedges[-1]
+        plt.axis( [xmin, xmax, ymin, ymax] )
+        axes = plt.axes()
+        axes.set_xlabel( hist1.xlabel )
+        axes.set_ylabel( hist1.ylabel )
+        hist2.contour( levels = options2["contours"], colors = options2["colors"], linewidths = 2 )
+        hist1.colz()
+        plt.axis( [xmin, xmax, ymin, ymax] )
+        plt.clim( *options1["zrange"] )
+        pylab.yticks(pylab.arange( ymin, ymax+0.1, options1["yticks"] ) )
+        pylab.xticks(pylab.arange( xmin, xmax+0.1, options1["xticks"] ) )
+        axes.set_title( options1["title"] )
+        plt.savefig( fig_name( options1, filename ) + "_col_con.%s" % ext )
 
 def makeGridPlots( histos, filename, ext="png" ) :
     # old code : doesnt owrk
