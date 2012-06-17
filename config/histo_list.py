0a0ee5f1d42bcc92c997eb055601f3c5560b031f
Keesjan de Vries <keesjan777@gmail.com>
2012-06-17 16:05:46 +0100

    work in progress

--- config/histo_list.py
+++ config/histo_list.py
@@ -1,4 +1,5 @@
 from config.plot_defaults import getDefaults as pd
+from collections import OrderedDict
 
 def getSpaceDict() :
     return spaces
@@ -11,6 +12,10 @@ def get1DDict() :
 
 def get_1d_overlay_dict() :
     return dlh_overlay
+
+def get_colour_contour_dict():
+    return colour, contour
+
 
 hists = {
 #            "data_histograms/iHist_neu1_dchi"      : pd( "dchi", "mneu1" ),
@@ -33,17 +38,17 @@ hists = {
 #            "data_histograms/iHist_tanb_m12_dchi" : pd("dchi", "tanb","m12" ),
 #         }
 spaces = { 
-#            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_m12_dX_Oh^2"     : pd("chi2", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_m12_pred_Bsmumu"     : pd("bsmm", "m0",  "m12" ),
+            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
+            "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
 #            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "m12" ),
 #            "data_histograms/iHist_m0_tanb_chi2"    : pd("chi2", "m0",  "m12" ),
 #            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
 #            "data_histograms/iHist_tanb_m12_chi2"   : pd("chi2", "tanb","m12" ),
 #            "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
 #            "data_histograms/iHist_mA0_tanb_chi2"   : pd("chi2", "MA"  ,"tanb"),
-            "data_histograms/iHist_mA0_tanb_pred_Bsmumu"   : pd("Pbsmm", "MA"  ,"tanb"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_dchi"   : pd("dchi", "mneu1"  ,"ssi"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_chi2"   : pd("chi2", "mneu1"  ,"ssi"),
+###            "data_histograms/iHist_mA0_tanb_pred_Bsmumu"   : pd("Pbsmm", "MA"  ,"tanb"),
 ##            "data_histograms/iHist_mneu1_ssi_dchi": pd("dchi","mneu1","ssi" ),
         }
 
@@ -53,4 +58,29 @@ dlh_overlay = {
 
 space_overlay = {
             "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
+            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "tanb" ),
+            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
+            "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
                 }
+#contour = OrderedDict()
+contour = [
+            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+          ]
+
+colour = OrderedDict()
+colour =[
+#          [  "data_histograms/iHist_m0_m12_pred_MW"          , pd("PMW", "m0"  ,"m12") ], 
+          [  "data_histograms/iHist_m0_m12_pred_Bsmumu"      , pd("Pbsmm", "m0"  ,"m12") ], 
+#          [  "data_histograms/iHist_m0_m12_pred_Oh^2"        , pd("POh2", "m0",  "m12" ) ],
+#          [  "data_histograms/iHist_m0_m12_pred_m_h^0"       , pd("Pm_h0", "m0",  "m12" )],
+
+#          [  "data_histograms/iHist_mA0_tanb_pred_MW"        , pd("PMW", "MA"  ,"tanb")],
+          [  "data_histograms/iHist_mA0_tanb_pred_Bsmumu"    , pd("Pbsmm", "MA"  ,"tanb")],
+#          [  "data_histograms/iHist_mA0_tanb_pred_Oh^2"      , pd("POh2", "MA",  "tanb" )],
+#          [  "data_histograms/iHist_mA0_tanb_pred_m_h^0"     , pd("Pm_h0", "MA"  ,"tanb")],
+        ]
