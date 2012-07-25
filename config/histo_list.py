eab80763a513832289b1fd00dc87072f4dbf46bd
Keesjan de Vries <keesjan777@gmail.com>
2012-07-25 13:51:25 +0100

    update

--- config/histo_list.py
+++ config/histo_list.py
@@ -1,6 +1,9 @@
 from config.plot_defaults import getDefaults as pd
 from collections import OrderedDict
 
+def get_higgs_plot():
+    return higgs_plot
+
 def getSpaceDict() :
     return spaces
 
@@ -17,70 +20,96 @@ def get_colour_contour_dict():
     return colour, contour
 
 
+grid_list={
+                
+            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
+            "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
+            "data_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
+            "data_histograms/iHist_gluino_chi2"     : pd("dchi", "mg"   ),
+          }
+
 hists = {
 #            "data_histograms/iHist_neu1_dchi"      : pd( "dchi", "mneu1" ),
+#            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi", "mh" ),
+            "data_histograms/iHist_stau_1_dchi"     : pd("dchi","mstau1"),
+            "data_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
+            "data_histograms/iHist_squark_r_dchi"  : pd("dchi", "msqr" ),
+            "data_histograms/iHist_BsmumuRatio_dchi"     : pd("dchi", "bsmmratio" ),
+##            "data_histograms/iHist_RatioBsmumu_dchi"     : pd("dchi", "bsmm" ),
+#           "data_histograms/iHist_stop1_dchi"  : pd("dchi", "stop1" ),
+        }
+
+spaces = { 
+            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
+##            "data_histograms/iHist_m0_m12_pred_m_h^0": pd("Pm_h0", "m0" ,"m12" ),
+##           "data_histograms/iHist_m0_m12_pred_Bsmumu":       pd("Pbsmm", "m0"  ,"m12") , 
+           "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
+            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "tanb" ),
+            "data_histograms/iHist_m0_tanb_chi2"    : pd("chi2", "m0",  "m12" ),
+            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
+            "data_histograms/iHist_tanb_m12_chi2"   : pd("chi2", "tanb","m12" ),
+            "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
+            "data_histograms/iHist_mA0_tanb_chi2"   : pd("chi2", "MA"  ,"tanb"),
+#            "data_histograms/iHist_mA0_tanb_pred_m_h^0"     : pd("Pm_h0", "MA"  ,"tanb"),
+#            "data_histograms/iHist_mA0_tanb_pred_Bsmumu"    : pd("Pbsmm", "MA"  ,"tanb"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_dchi"   : pd("dchi", "mneu1"  ,"ssi"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_chi2"   : pd("chi2", "mneu1"  ,"ssi"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_cm-2_dchi"   : pd("dchi", "mneu1"  ,"ssicm"),
+#            "data_histograms/iHist_neu1_sigma_pp^SI_cm-2_chi2"   : pd("chi2", "mneu1"  ,"ssicm"),
+####            "data_histograms/iHist_mA0_tanb_pred_Bsmumu"   : pd("Pbsmm", "MA"  ,"tanb"),
+##            "data_histograms/iHist_mneu1_ssi_dchi": pd("dchi","mneu1","ssi" ),
+        }
+
+contour = [
+#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
+#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_neu1_sigma_pp^SI_cm-2_dchi"   , pd("dchi", "mneu1"  ,"ssicm")],
+          ]
+
+colour =[
+#          [  "data_histograms/iHist_m0_m12_pred_MW"          , pd("PMW", "m0"  ,"m12") ], 
+#          [  "data_histograms/iHist_m0_m12_pred_Bsmumu"      , pd("Pbsmm", "m0"  ,"m12") ], 
+#          [  "data_histograms/iHist_m0_m12_pred_tanb"       , pd("Ptanb", "m0"  ,"m12") ], 
+#           [ "data_histograms/iHist_m0_m12_chi2"     , pd("chi2", "m0",  "m12" ),]
+#          [  "data_histograms/iHist_m0_m12_pred_Oh^2"        , pd("POh2", "m0",  "m12" ) ],
+#          [  "data_histograms/iHist_m0_m12_pred_m_h^0"       , pd("Pm_h0", "m0",  "m12" )],
+#          [  "data_histograms/iHist_mA0_tanb_pred_MW"        , pd("PMW", "MA"  ,"tanb")],
+#          [  "data_histograms/iHist_mA0_tanb_pred_Bsmumu"    , pd("Pbsmm", "MA"  ,"tanb")],
+#          [  "data_histograms/iHist_mA0_tanb_pred_Oh^2"      , pd("POh2", "MA",  "tanb" )],
+#          [  "data_histograms/iHist_mA0_tanb_pred_m_h^0"     , pd("Pm_h0", "MA"  ,"tanb")],
+#            ["data_histograms/iHist_neu1_sigma_pp^SI_cm-2_chi2"   , pd("chi2", "mneu1"  ,"ssicm")],
+        ]
+
+
+
+
+
+
+
+higgs_plot =       {
+            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi_red_band", "mh" ),
+#            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi", "mh" ),
+        }
+dlh_overlay = {
+#              "data_histograms/iHist_BsmumuRatio_dchi"     : pd("dchi", "bsmmratio" ),
+##              "data_histograms/iHist_Bsmumu_dchi"     : pd("dchi", "bsmm" ),
 #            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi", "mh" ),
 #            "data_histograms/iHist_stau_1_dchi"     : pd("dchi","mstau1"),
 #            "data_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
 #            "data_histograms/iHist_squark_r_dchi"  : pd("dchi", "msqr" ),
-#            "data_histograms/iHist_Bsmumu_dchi"     : pd("dchi", "bsmm" ),
-        }
-
-#spaces = { 
-#            "data_histograms/iHist_m0_m12_chi2"   : pd("chi2", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_m12_pval"   : pd("pval", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_m12_dchi"   : pd("dchi", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_tanb_chi2"  : pd("chi2", "m0",  "tanb"),
-#            "data_histograms/iHist_m0_tanb_pval"  : pd("pval", "m0",  "tanb"),
-#            "data_histograms/iHist_m0_tanb_dchi"  : pd("dchi", "m0",  "tanb"),
-#            "data_histograms/iHist_tanb_m12_chi2" : pd("chi2", "tanb","m12" ),
-#            "data_histograms/iHist_tanb_m12_pval" : pd("pval", "tanb","m12" ),
-#            "data_histograms/iHist_tanb_m12_dchi" : pd("dchi", "tanb","m12" ),
-#         }
-spaces = { 
-            "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
-            "data_histograms/iHist_m0_m12_chi2"     : pd("chi2", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "m12" ),
-#            "data_histograms/iHist_m0_tanb_chi2"    : pd("chi2", "m0",  "m12" ),
-#            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
-#            "data_histograms/iHist_tanb_m12_chi2"   : pd("chi2", "tanb","m12" ),
-#            "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
-#            "data_histograms/iHist_mA0_tanb_chi2"   : pd("chi2", "MA"  ,"tanb"),
-#            "data_histograms/iHist_neu1_sigma_pp^SI_dchi"   : pd("dchi", "mneu1"  ,"ssi"),
-#            "data_histograms/iHist_neu1_sigma_pp^SI_chi2"   : pd("chi2", "mneu1"  ,"ssi"),
-###            "data_histograms/iHist_mA0_tanb_pred_Bsmumu"   : pd("Pbsmm", "MA"  ,"tanb"),
-##            "data_histograms/iHist_mneu1_ssi_dchi": pd("dchi","mneu1","ssi" ),
-        }
-
-dlh_overlay = {
-                "data_histograms/iHist_Bsmumu_dchi"     : pd("dchi", "bsmm" ),
+#            "data_histograms/iHist_stop1_dchi"  : pd("dchi", "stop1" ),
+##                "data_histograms/iHist_neu1_dchi"     : pd("dchi", "mneu1" ),
                }
 
 space_overlay = {
             "data_histograms/iHist_m0_m12_dchi"     : pd("dchi", "m0",  "m12" ),
-            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "tanb" ),
-            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
+#            "data_histograms/iHist_m0_tanb_dchi"    : pd("dchi", "m0",  "tanb" ),
+#            "data_histograms/iHist_tanb_m12_dchi"   : pd("dchi", "tanb","m12" ),
             "data_histograms/iHist_mA0_tanb_dchi"   : pd("dchi", "MA"  ,"tanb"),
+##            "data_histograms/iHist_neu1_sigma_pp^SI_cm-2_dchi"   : pd("dchi", "mneu1"  ,"ssicm"),
                 }
 #contour = OrderedDict()
-contour = [
-            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
-#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
-#            ["data_histograms/iHist_m0_m12_dchi"     , pd("dchi", "m0",  "m12" )],
-            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
-#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
-#            ["data_histograms/iHist_mA0_tanb_dchi"   , pd("dchi", "MA"  ,"tanb")],
-          ]
-
-colour = OrderedDict()
-colour =[
-#          [  "data_histograms/iHist_m0_m12_pred_MW"          , pd("PMW", "m0"  ,"m12") ], 
-          [  "data_histograms/iHist_m0_m12_pred_Bsmumu"      , pd("Pbsmm", "m0"  ,"m12") ], 
-#          [  "data_histograms/iHist_m0_m12_pred_Oh^2"        , pd("POh2", "m0",  "m12" ) ],
-#          [  "data_histograms/iHist_m0_m12_pred_m_h^0"       , pd("Pm_h0", "m0",  "m12" )],
-
-#          [  "data_histograms/iHist_mA0_tanb_pred_MW"        , pd("PMW", "MA"  ,"tanb")],
-          [  "data_histograms/iHist_mA0_tanb_pred_Bsmumu"    , pd("Pbsmm", "MA"  ,"tanb")],
-#          [  "data_histograms/iHist_mA0_tanb_pred_Oh^2"      , pd("POh2", "MA",  "tanb" )],
-#          [  "data_histograms/iHist_mA0_tanb_pred_m_h^0"     , pd("Pm_h0", "MA"  ,"tanb")],
-        ]
--- config/plot_defaults.py
+++ config/plot_defaults.py
@@ -11,25 +11,30 @@ ticks = {
           "m0"    :1000,
           "m12"   : 500,
           "tanb"  : 10,
-          "mneu1" : 500,
-          "ssi"   : 10e-40,
+          "mneu1" : 100,
+          "ssi"   : 10e-12,
           "mh"    : 5,
           "MA"    : 500,
-          "mstau1": 500,
-          "mg"    : 500,
-          "msqr"  : 500,
-          "bsmm"  : 2e-9,
+          "mstau1": 1000,
+          "mg"    : 1000,
+          "msqr"  : 1000,
+          "stop1"  : 1000,
+          "bsmm"  : 1.0e-9,
+          "bsmmratio"  : 0.5,
         }
 
 m0m12Base   = { "xticks" : ticks["m0"],   "yticks" : ticks["m12"],  }
 m0tanbBase  = { "xticks" : ticks["m0"],   "yticks" : ticks["tanb"], }
 tanbm12Base = { "xticks" : ticks["tanb"], "yticks" : ticks["m12"],  }
 MAtanbBase  = { "xticks" : ticks["MA"  ], "yticks" : ticks["tanb"], }
-mneu1ssiBase= { "xticks" : ticks["mneu1"],"yticks" : ticks["ssi"],  }
+mneu1ssiBase= { "ylog" : True, "xlog" : True}
+mneu1ssicmBase= { "ylog" : True, "xlog" : True}
 mstau1Base  = { "xticks" : ticks["mstau1"],}
 mgBase      = { "xticks" : ticks["mg"],    }
 msqrBase    = { "xticks" : ticks["msqr"],  }
+stop1Base    = { "xticks" : ticks["stop1"],  }
 bsmmBase    = { "xticks" : ticks["bsmm"],  }
+bsmmratioBase    = { "xticks" : ticks["bsmmratio"],  }
 mhBase      = { "xticks" : ticks["mh"], }
 mneu1Base   = { "xticks" : ticks["mneu1"], }
 
@@ -38,7 +43,7 @@ colors  = [ 'r', 'b' ]
 
 chi2Base = { 
                "title"    : r"$\chi^{2}$",
-               "contours" : [22.23, 25.99], # need more sensible values
+               "contours" : [0.   , 0.   ], # need more sensible values
                "colors"   : colors, 
                "zrange"   : [20., 45.]
            }
@@ -52,7 +57,15 @@ pvalBase = {
 
 dchiBase = {
                 "title"    : r"$\Delta\chi^{2}$",
-                "contours" : [2.23, 5.99],
+                "contours" : [2.30, 5.99],
+                "colors"   : colors,
+                "zrange"   : [0.,  7.],
+                "zrange1d" : [0., 9.],
+           }
+
+dchi_red_bandBase = {
+                "title"    : r"$\Delta\chi^{2}$",
+                "contours" : [2.30, 5.99],
                 "colors"   : colors,
                 "zrange"   : [0.,  7.],
                 "zrange1d" : [0., 9.],
@@ -60,8 +73,39 @@ dchiBase = {
 
 PbsmmBase = {
                 "title"    : r"Prediction for $BR(B_{s}\rightarrow\mu\mu) $  ",
-                "contours" : [2.23, 5.99],
+                "contours" : [3.46e-9, 1],
                 "colors"   : colors,
                 "zrange"   : [0., 6.92e-9  ],
-                "zrange1d" : [0., 9.],
+                "zrange1d" : [0., 6.92e-9  ],
+           }
+
+POh2Base = {
+                "title"    : r"Prediction for $\Omega_{CDM}h^2$  ",
+#                "contours" : [2.23, 5.99],
+                "colors"   : colors,
+                "zrange"   : [0.090, 0.130   ], # 0.1109 +- ~ (7 x the standard deviation) ! !
+                "zrange1d" : [0.075, 0.175   ],
+           }
+
+Pm_h0Base = {
+                "title"    : r"Prediction for $M_{h} $  ",
+                "contours" : [125,0],
+                "colors"   : colors,
+                "zrange"   : [122.,128.    ],
+                "zrange1d" : [115.,130.    ],
+           }
+
+PMWBase = {
+                "title"    : r"Prediction for $M_{W} $  ",
+#                "contours" : [2.23, 5.99],
+                "colors"   : colors,
+                "zrange"   : [80.34834, 80.42106    ],
+                "zrange1d" : [115.,130.    ],
+           }
+PtanbBase = {
+                "title"    : r"Value of $\tan\beta $  ",
+                "contours" : [0, 40],
+                "colors"   : colors,
+                "zrange"   : [0,60    ],
+                "zrange1d" : [0.,60.    ],
            }
