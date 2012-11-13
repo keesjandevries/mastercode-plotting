1d895a1704c7fe94e727bac28f4fe4ee4c9c3320
Keesjan de Vries <keesjan777@gmail.com>
2012-11-13 17:09:48 +0000

    add in smooth_test_list

---
+++ config/smooth_test_list.py
@@ -0,0 +1,29 @@
+from config.plot_defaults import getDefaults as pd
+from collections import OrderedDict
+
+
+def get_raw_smooth():
+    return raw_smooth
+
+
+raw_smooth=[
+#            (["data_histograms/iHist_stau_1_dchi"       ,  pd("dchi","mstau1")],
+#             ["smooth_histograms/iHist_stau_1_dchi"     ,  pd("dchi","mstau1")] ),
+#            (["data_histograms/iHist_stop1_dchi"       ,  pd("dchi","stop1")],
+#             ["smooth_histograms/iHist_stop1_dchi"     ,  pd("dchi","stop1")] ),
+#            (["data_histograms/iHist_gluino_dchi"       ,  pd("dchi","mg")],
+#             ["smooth_histograms/iHist_gluino_dchi"     ,  pd("dchi","mg")] ),
+            (["data_histograms/iHist_BsmumuRatio_dchi"  ,  pd("dchi", "bsmmratio" )],
+             ["smooth_histograms/iHist_BsmumuRatio_dchi",  pd("dchi", "bsmmratio" )] ),
+#           (["data_histograms/iHist_squark_r_dchi"       ,  pd("dchi","msqr")],
+#            ["smooth_histograms/iHist_squark_r_dchi"     ,  pd("dchi","msqr")] ),
+#            (["data_histograms/iHist_m_h^0_dchi"       ,  pd("dchi_red_band","mh")],
+#             ["smooth_histograms/iHist_m_h^0_dchi"     ,  pd("dchi_red_band","mh")] ),
+#            (["data_histograms/iHist_Dm_stau1_neu1_dchi" ,      pd( "dchi", "Dm_stau1_neu1" )],
+#             ["smooth_histograms/iHist_Dm_stau1_neu1_dchi" ,      pd( "dchi", "Dm_stau1_neu1" )])
+#            (["data_histograms/iHist_Dm_nslp_lsp_dchi" ,      pd( "dchi", "Dm_nslp_lsp" )],
+#             ["smooth_histograms/iHist_Dm_nslp_lsp_dchi" ,      pd( "dchi", "Dm_nslp_lsp" )])
+               
+                ]
+
+
