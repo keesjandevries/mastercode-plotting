9620e848e9dd99ba0532d82d1f07da4e43a950a8
Keesjan de Vries <keesjan777@gmail.com>
2012-11-13 15:34:00 +0000

    overlay_lhood cleanup

---
+++ config/overlay_lhoods_list.py
@@ -0,0 +1,23 @@
+from config.plot_defaults import getDefaults as pd
+from collections import OrderedDict
+
+
+def get_1d_overlay_dict() :
+    return dlh_overlay
+
+dlh_overlay = {
+#              "data_histograms/iHist_Bsmumu_dchi"     : pd("dchi", "bsmm" ),
+#            "data_histograms/iHist_m_h^0_dchi"      : pd( "dchi", "mh" ),
+#            "data_histograms/iHist_stau_1_dchi"     : pd("dchi","mstau1"),
+#            "data_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
+#            "data_histograms/iHist_squark_r_dchi"  : pd("dchi", "msqr" ),
+#            "data_histograms/iHist_stop1_dchi"  : pd("dchi", "stop1" ),
+            "smooth_histograms/iHist_BsmumuRatio_dchi"     : pd("dchi", "bsmmratio" ),
+#            "smooth_histograms/iHist_stau_1_dchi"     : pd("dchi","mstau1"),
+            "smooth_histograms/iHist_gluino_dchi"     : pd("dchi", "mg"   ),
+#            "smooth_histograms/iHist_squark_r_dchi"  : pd("dchi", "msqr" ),
+#            "smooth_histograms/iHist_stop1_dchi"  : pd("dchi", "stop1" ),
+#              "data_histograms/iHist_BsmumuRatio_dchi"     : pd("dchi", "bsmmratio" ),
+#                "data_histograms/iHist_neu1_dchi"     : pd("dchi", "mneu1" ),
+               }
+
---
+++ overlay_lhoods.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+from config import histo_list as hl
+from modules import spaces as s
+from modules import lh1d as lh
+
+from sys import argv
+
+def main() :
+    spaces = hl.get_space_overlay_dict()
+    histos = hl.get_1d_overlay_dict()
+    files =  argv[1:] 
+
+    s.make_single_space_overlay(spaces, files , "eps")
+    lh.make_single_1d_overlay(histos, files, "eps")
+
+if __name__ == "__main__":
+    main()
