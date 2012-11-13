5365d9394471df1931b62f832d19e1bdeb14db67
Keesjan de Vries <keesjan777@gmail.com>
2012-11-13 15:25:05 +0000

    spaces cleaned up

--- spaces.py
+++ spaces.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
-from config import histo_list as hl
+from config import spaces_list as hl
 from modules import spaces as s
 
 from sys import argv
 
 def main() :
-    spaces = hl.getSpaceDict()
+    spaces = hl.get_spaces_dict()
     for f in argv[1:] :
-        s.makeSingleSpacePlot(spaces, f, "pdf")
+        s.make_single_space_plot(spaces, f, "pdf")
 
 if __name__ == "__main__":
     main()
